c2070ca8edd4c09f248b5ef3c00fc9468874cf54
mohammadshabana1824 <102870979+mohammadshabana1824@users.noreply.github.com>
2025-01-10 20:46:21 +0530

    counter app uploaded sucessfully

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+const Counter=()=>{
+    const [value,setValue]=useState(0)
+    const handleIncrement=()=>{
+            setValue(value+1);
+    }
+    const handleDecrement=()=>{
+            setValue(value-1);
+    }
+return (
+    <div>
+        <h1>Counter App</h1>
+        <p>count:{value}</p>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+    </div>
+)
+}
+export default Counter;
